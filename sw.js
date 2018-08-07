e989c82d105bf46ae27c251d4edfdbacc26ae313
gauravsinghaec <gauravsinghaec@gmail.com>
2018-08-07 17:36:15 +0530

    feat: Change homepage relative path for github project page.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ var staticCacheName = 'mws-static-v1';
  */
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(staticCacheName).then((cache) => {
-    return cache.addAll(['/', 'js/main.js', 'js/restaurant_info.js', 'js/dbhelper.js', 'css/styles.css',
+    return cache.addAll(['./', 'js/main.js', 'js/restaurant_info.js', 'js/dbhelper.js', 'css/styles.css',
       'img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg',
       'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
       'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
